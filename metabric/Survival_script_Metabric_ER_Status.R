181f87c32c5e4efda32c727d7a339edba26dc92e
AKMB <mcanne1395@gmail.com>
2023-10-13 09:32:58 +0200

    cleaned script and removed everything that was not relevant for ER DSS

--- metabric/Survival_script_Metabric_ER_Status.R
+++ metabric/Survival_script_Metabric_ER_Status.R
@@ -2,8 +2,8 @@
 library(tibble)
 library(survival)
 library(survminer)
-
-#Upload TCGA file 
+setwd("C:/Users/abe186/UiT Office 365/O365-Bioinformatikk TRIM27 - General/Metabric")
+#Upload METABRIC file 
 ER <- read.csv2("Ensembl_Metabric_17724_duplicates removed .csv",sep = ";", as.is = T, check.names = F)
 ER <- ER[,-c(1,3)]
 
@@ -20,10 +20,10 @@ info <- read.csv2("brca_metabric_clinical_data.csv",sep = ";", as.is = T, check.
 info_rec <- info[,-c(1,3:11)]
 info_rec <- info_rec[,-c(2,4,5,7:29)]
 
-#Merge the genes and infor file based on patient ID 
+#Merge the genes and info file based on patient ID 
 genes <- tibble::rownames_to_column(genes, "ID")
 merged <- merge(genes, info_rec, by.x= "ID", by.y = "Patient ID")
-write.csv2(merged, "TRIM45_receptor_status_Metabric.csv")
+#write.csv2(merged, "TRIM45_receptor_status_Metabric.csv")
 #merged<- read.csv2("TRIM45_receptor status_TCGA_clinical.csv",sep = ";", as.is = T, check.names = F)
 #merged <- merged[,-1]
 
@@ -31,138 +31,54 @@ write.csv2(merged, "TRIM45_receptor_status_Metabric.csv")
 info_dss <- info[,c(2,26, 27,30,31,39)]
 write.csv2(info_dss, "DSS_PFI_info_Metabric.csv")
 
-merged<- read.csv2("TRIM45_receptor_status_Metabric.csv",sep = ";", as.is = T, check.names = F)
-dss<- read.csv2("DSS_Metabric.csv",sep = ";", as.is = T, check.names = F)
-pfi<- read.csv2("PFI_Metabric.csv",sep = ";", as.is = T, check.names = F)
+#merged<- read.csv2("TRIM45_receptor_status_Metabric.csv",sep = ";", as.is = T, check.names = F)
+#dss<- read.csv2("DSS_Metabric.csv",sep = ";", as.is = T, check.names = F)
+
 
 #Merge the merged file and the dss/pfi file 
-ER_DSS <- merge(merged, dss, by.x= "ID", by.y = "Patient ID")
-ER_PFI <- merge(merged, pfi, by.x= "ID", by.y = "Patient ID")
-
+ER_DSS <- merge(merged, dss, by.x= "ID", by.y = "Patient.ID")
 
 # 1)Subset ER_DSS and ER_PFI based on positive ER status 
 ER_DSS_pos <- subset(ER_DSS, ER_DSS$`ER Status` %in% "Positive")
-ER_PFI_pos <- subset(ER_PFI, ER_PFI$`ER Status` %in% "Positive")
-
-
-# 2) You can also subset the df based on HER2 negative status 
-ER_DSS_her <- subset(ER_DSS_pos, ER_DSS_pos$`HER2 Status` %in% "Negative")
-ER_PFI_her <- subset(ER_PFI_pos, ER_PFI_pos$`HER2 Status` %in% "Negative")
-
 
 #Define TRIM45 expression after subsetting based on receptor status
 ER_DSS_pos$TRIM45_expression <- ifelse(ER_DSS_pos$TRIM45 >= median(ER_DSS_pos$TRIM45), 'High', "Low")
-ER_DSS_her$TRIM45_expression <- ifelse(ER_DSS_her$TRIM45 >= median(ER_DSS_her$TRIM45), 'High', "Low")
-
-ER_PFI_pos$TRIM45_expression <- ifelse(ER_PFI_pos$TRIM45 >= median(ER_PFI_pos$TRIM45), 'High', "Low")
-ER_PFI_her$TRIM45_expression <- ifelse(ER_PFI_her$TRIM45 >= median(ER_PFI_her$TRIM45), 'High', "Low")
-
 
 #Remove timepoints that start at 0 
 ER_DSS_pos <- subset(ER_DSS_pos, ER_DSS_pos$DSS.months > 0)
-ER_DSS_her <- subset(ER_DSS_her, ER_DSS_her$DSS.months > 0)
-
-ER_PFI_pos <- subset(ER_PFI_pos, ER_PFI_pos$PFI.months > 0)
-ER_PFI_her <- subset(ER_PFI_her, ER_PFI_her$PFI.months > 0)
-
 
 #Convert DSS/PFI.time into years 
 ER_DSS_pos$years <- ER_DSS_pos$DSS.months/12
-ER_DSS_her$years <- ER_DSS_her$DSS.months/12 
-
-ER_PFI_pos$years <- ER_PFI_pos$PFI.months/12
-ER_PFI_her$years <- ER_PFI_her$PFI.months/12
 
 #Define survival
 survival_DSS_pos = Surv(time= ER_DSS_pos$years, event = ER_DSS_pos$DSS)
-survival_DSS_her = Surv(time= ER_DSS_her$years, event = ER_DSS_her$DSS)
-
-survival_PFI_pos = Surv(time = ER_PFI_pos$years, event= ER_PFI_pos$PFI)
-survival_PFI_her = Surv(time = ER_PFI_her$years, event= ER_PFI_her$PFI)
-
 
 survival_fit_DSS_pos<- survfit(formula = survival_DSS_pos ~ ER_DSS_pos$TRIM45_expression, data = ER_DSS_pos)
-survival_fit_DSS_her<- survfit(formula = survival_DSS_her ~ ER_DSS_her$TRIM45_expression, data = ER_DSS_her)
 
-survival_fit_PFI_pos<- survfit(formula = survival_PFI_pos ~ ER_PFI_pos$TRIM45_expression, data = ER_PFI_pos)
-survival_fit_PFI_her<- survfit(formula = survival_PFI_her ~ ER_PFI_her$TRIM45_expression, data = ER_PFI_her)
+#Make plot
+p <- ggsurvplot(fit = survival_fit_DSS_pos, 
+                pval = TRUE, 
+                legend = "right",
+                title = "ER-Positive Patients",
+                xlab = "Years", 
+                ylab = "DSS Probability", 
+                ylim=c(0.1,1), xlim=c(0,30), 
+                pval.coord=c(0,0.12), 
+                conf.int = T, risk.table = T, 
+                legend.labs=c("High TRIM45", "Low TRIM45"),
+                legend.title="TRIM45 Expression",
+                palette = c("#E69F00","#0072B2"),
+                risk.table.y.text = TRUE, risk.table.title="",
+                risk.table.height=0.15, 
+                risk.table.fontsize=3.5, 
+                tables.theme = theme(axis.text.x = element_blank(), axis.ticks.x=element_blank(), 
+                                     axis.title.x = element_blank(), axis.title.y = element_blank(),
+                                     axis.text.y = element_text(size = 10)),font.x=10, font.y=11, font.tickslab=10)
 
+p
 
- ggsurv <- ggsurvplot(fit= survival_fit_DSS_pos, 
-                      pval = TRUE, 
-                      surv.median.line = "hv", 
-                      xlab = "DSS (Years)", 
-                      ylab = "DSS Probability",
-                      ylim=c(0.0,1), 
-                      xlim=c(0,30),
-                      palette = c("#E69F00","#0072B2", "#009E73", "#CC79A7"),
-                      pval.coord=c(0.1,0.1), 
-                      break.x.by= 5,         
-                      conf.int = T, 
-                      conf.int.alpha = c(0.3), 
-                      conf.int.style="ribbon",
-                      risk.table = T,
-                      risk.table.height = 0.15,
-                      ncensor.plot = F,
-                      ncensor.plot.height = 0.15,
-                      #legend = c(0.5, 0.95),
-                      legend.labs=c("High TRIM45","Low TRIM45"),
-                      legend.title= "ER+",font.x=c("12"), 
-                      font.y= c("12"), 
-                      font.legend=c("12"), 
-                      pval.font= c("12"))
-#?ggsurvplot
-ggsurv$plot <- ggsurv$plot+
-  theme(axis.text.y = element_text(size = 12),
-        axis.text.x = element_text(size=12))
-
-ggsurv$table <- ggsurv$table+
-  theme(plot.title = element_text(size = 12),
-        axis.text.x = element_text(size=12),
-        axis.title.x.bottom = element_text(size=12), 
-        axis.title.y.left = element_text(size=12), 
-        legend.title = element_text(size=12),
-        legend.text = element_text(size=12))
-
-
-ggsurv$ncensor.plot <-ggsurv$ncensor.plot+
-  theme( plot.title = element_text(size = 12),
-         axis.text.x = element_text(size=12),
-         axis.text.y = element_text(size=12), 
-         axis.title.x.bottom = element_text(size=12),
-         axis.title.y.left = element_text(size=12))
-
-ggsurv
-
-pdf("TRIM45_ER_pos_DSS_metabric.pdf", height = 8, width = 8)
-print(ggsurv)
+pdf("TRIM45_ER_pos_DSS_metabric_2.pdf", width = 8, height = 7, onefile = F)
+print(p)
 dev.off()
-
-pfi_her <- ggsurvplot(fit= survival_fit_PFI_her, 
-                      pval = TRUE, 
-                      surv.median.line = "hv", 
-                      xlab = "PFI (Years)", 
-                      ylab = "PFI Probability",
-                      ylim=c(0.1,1), 
-                      xlim=c(0,30),
-                      palette = c("#E69F00","#0072B2", "#009E73", "#CC79A7"),
-                      pval.coord=c(0.1,0.12), 
-                      break.x.by= 5,         
-                      conf.int = T, 
-                      conf.int.alpha = c(1), 
-                      conf.int.style="step",
-                      risk.table = T,
-                      risk.table.height = 0.15,
-                      ncensor.plot = TRUE,
-                      ncensor.plot.height = 0.15,
-                      #legend = c(0.5, 0.95),
-                      legend.labs=c("High TRIM45", "Low TRIM45"),
-                      legend.title= "ER+ HER2-")
-
-
-
-
-list <- list(dss_er, dss_her, pfi_er, pfi_her)
-ggexport(list, filename = "TRIM45_ER_pos_HER_neg_DSS_PFI_V2.pdf", height= 15, width = 15)
 
 
