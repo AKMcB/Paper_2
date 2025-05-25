cbd2b10bf9d716b290f0fc28dddf59a6e12f7162
Anne Berge <118718589+AKMcB@users.noreply.github.com>
2025-05-25 16:49:48 +0200

    Update Breast cancer cell lines_script_V4_boxplot.R

--- CCLE/Breast cancer cell lines_script_V4_boxplot.R
+++ CCLE/Breast cancer cell lines_script_V4_boxplot.R
@@ -1,15 +1,15 @@
-####################
-## Load libraries ##
-####################
+##################
+# Load libraries #
+##################
 
 library(ggpubr)
 library(tidyverse)
 
-##########################
-## Load expression data ##
-##########################
+########################
+# Load expression data #
+########################
 
-setwd("C:/Users/abe186/UiT Office 365/O365-Bioinformatikk TRIM27 - General/Cancer cell lines cyclopedia/Data files")
+setwd("C:/Users/abe186/UiT Office 365/O365-Phd Anne - General/Cancer cell lines cyclopedia/Data files")
 expr <- read.csv2("BCCL_filtered_log2.csv", sep=";", as.is = T, check.names = F)
 
 genes<- subset(expr, expr$Name %in% c("TRIM45", "TRIM27", "TRIM32"))
@@ -20,9 +20,9 @@ t_genes<- t(genes)
 t_genes<- as.data.frame(t_genes)
 t_genes <- tibble::rownames_to_column(t_genes, "id")
 
-######################
-## Read sample info ##
-######################
+####################
+# Read sample info #
+####################
 
 ids <- read.csv("sample_info.csv", sep=";", as.is = T, check.names = F)
 ids<- ids[ ,c(2,4,21)]
@@ -36,7 +36,7 @@ rownames(merged)<-merged[,1]
 merged<- merged[,-1]
 str(merged)
 
-merged$lineage_molecular_subtype      <- factor(merged$lineage_molecular_subtype, 
+merged$lineage_molecular_subtype  <- factor(merged$lineage_molecular_subtype, 
                                               levels= c("luminal","HER2_amp", "basal_A", "basal_B"), 
                                               labels = c("Luminal","HER2-enriched", "Basal-like A", "Basal-like B" ))
 
@@ -48,8 +48,6 @@ symnum.args <- list(cutpoints = c(0, 0.0001, 0.001, 0.01, 0.05, 1),
                     symbols = c("****", "***", "**", "*", "ns"))
 
 colnames(merged)[5] <- "PAM50"
-
-
 
 p <- ggplot(merged, aes(x = PAM50, y = TRIM45, fill = PAM50))+
   geom_point(alpha=0.5,position = position_jitter(width = 0.3, height = 0.5), shape= 21, size= 3)+
@@ -73,7 +71,3 @@ p
 pdf("TRIM45_exp_BCCL_PAM50_3.pdf", height = 6, width = 6)
 print(p)
 dev.off()
-
-
-ggexport(p, filename = "TRIM45_exp_BCCL_PAM50.pdf", res = 200, height = 2000, width = 2000)
-
