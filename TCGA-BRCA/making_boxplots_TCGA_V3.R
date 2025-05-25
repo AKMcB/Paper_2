ac0c92a26a3bdacc0dcfa223bd9df96e7ad9a53b
Anne Berge <118718589+AKMcB@users.noreply.github.com>
2025-05-25 16:52:24 +0200

    Update making_boxplots_TCGA_V3.R

--- TCGA-BRCA/making_boxplots_TCGA_V3.R
+++ TCGA-BRCA/making_boxplots_TCGA_V3.R
@@ -1,14 +1,14 @@
-####################
-## Load libraries ##
-####################
+##################
+# Load libraries #
+##################
 
 library(ggpubr)
 library(tidyverse)
 
-##########################
-## Load expression data ##
-##########################
-setwd("C:/Users/abe186/UiT Office 365/O365-Bioinformatikk TRIM27 - General/TCGA-BRCA/Raw data")
+########################
+# Load expression data #
+########################
+setwd("C:/Users/abe186/UiT Office 365/O365-PhD Anne - General/TCGA-BRCA/Raw data")
 expr <- read.csv2("gdc_brca_expr_edit.csv", as.is = TRUE, check.names = FALSE)
 expr <- expr[,-1]
 expr <- distinct(expr,gene,.keep_all = T)
@@ -27,15 +27,12 @@ genes1 <- genes1[,-1]
 #t-transpose data t(x)
 t_genes1 <- t(genes1)
 
-
 #Convert it into a dataframe and add a column with the patient ids
 t_genes1 <- as.data.frame(t_genes1)
 #t_genes$id <- rownames(t_genes)
 t_genes1<- tibble::rownames_to_column(t_genes1, "id")
 
 #merged <- merge(t_genes_interactors, t_genes1, by.x = "id", by.y = "id")
-
-
 #write.csv(merged, "TRIM_interactors_autophagy.csv")
 
 ##Import the info file
@@ -54,7 +51,7 @@ merged <- merge(t_genes1, ids, by.x = "id", by.y = "TCGA_id")
 rownames(merged) <- merged[,1]
 merged<- merged[, -1]
 
-#write.csv(merged, "TRIM27_autophagy_receptors_TCGA.csv")
+table(merged$BRCA_Subtype_PAM50)
 
 #Make the boxplot
 merged$BRCA_Subtype_PAM50    <- factor(merged$BRCA_Subtype_PAM50, 
@@ -90,15 +87,8 @@ p <- ggplot(merged, aes(x = PAM50, y = TRIM45, fill = PAM50))+
   scale_fill_manual(values = c("#00AFBB", "#E7B800", "#FC4E07", "#A0D636","#DF2DE0","#333ED4"))+
   geom_signif(comparisons = my_comparisons,map_signif_level = T, y_position = c(7.0, 7.4,7.8, 5.9,6.3,6.7,5.9), textsize=10)
   
-
-
 p 
 
 pdf("TRIM45_exp_TCGA_BRCA_PAM50_3.pdf", height = 6, width = 6)
 print(p)
 dev.off()
-
-
-
-
-
